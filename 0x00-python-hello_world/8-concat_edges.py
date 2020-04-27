ff06fdd4de4ed39aeeb2df13eb4c579227ecb4e6
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-04-27 19:17:55 +0000

    fix excercise

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[str.find('object'):str.find('language')] + str[str.find('with'):str.find('with') + 5] + str[:6]
+str1 = str[str.find('object'):str.find('language')]
+str = str1 + str[str.find('with'):str.find('with') + 5] + str[:6]
 print(str)
