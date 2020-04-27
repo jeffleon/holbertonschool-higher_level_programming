7c1533fbfa2940a530c36f0ebbb13b4a4ce4769d
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-04-27 21:11:05 +0000

    Advanced

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -3,3 +3,4 @@ import sys
 
 str = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
 sys.stderr.write(str)
+exit(1)
--- 0x00-python-hello_world/102-magic_calculation.py
+++ 0x00-python-hello_world/102-magic_calculation.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-import dis
+
 def magic_calculation(one,two):
     return (98 + (one ** two))
-dis.dis(magic_calculation)
