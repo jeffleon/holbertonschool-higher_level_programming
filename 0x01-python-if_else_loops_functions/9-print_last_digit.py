7711245cfe11309567c8c30834fc88de145f85ce
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-04-28 21:35:00 +0000

    fix

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -3,8 +3,3 @@ def print_last_digit(number):
     num_ = number % 10 if number >= 0 else (number * -1) % 10
     print("{:d}".format(num_), end='')
     return(num_)
-
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
