b1c70b01a8a0a00c013e23592037b0bc5681be1d
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-04-28 23:13:44 +0000

    scripts

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    return(str[:n] + str[n+1:])
+    aux = ""
+    return str[:n] + str[n + 1:] if str is not None else aux
+
+print(remove_char_at("Holberton School", 3))
+print(remove_char_at("Chicago", 2))
+print(remove_char_at("C is fun!", 0))
+print(remove_char_at("School", 10))
+print(remove_char_at("Python", -2))
