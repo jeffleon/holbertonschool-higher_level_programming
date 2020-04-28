5d525794ec54e65fa09ff43b83ed7a021f374d18
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-04-28 21:04:28 +0000

    fix

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 10):
     for j in range(i + 1, 10):
         if i != 8:
-            print(str(i) + str(j), end=', ')
+            print("{}{}".format(i,j), end=', ')
         else:
-            print(str(i) + str(j))
+            print("{}{}".format(i,j))
