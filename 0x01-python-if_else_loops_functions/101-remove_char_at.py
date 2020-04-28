3a3005c0858912fbb1071b26a0caf11dfa1289fa
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-04-28 23:17:34 +0000

    fix

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,9 +2,3 @@
 def remove_char_at(str, n):
     aux = ""
     return str[:n] + str[n + 1:] if str is not None else aux
-
-print(remove_char_at("Holberton School", 3))
-print(remove_char_at("Chicago", 2))
-print(remove_char_at("C is fun!", 0))
-print(remove_char_at("School", 10))
-print(remove_char_at("Python", -2))
