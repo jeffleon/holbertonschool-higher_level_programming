7641ed4124289127577b0fa072ddcde52b2b9901
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-05-01 04:32:53 +0000

    fix task

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -10,7 +10,7 @@ def main():
         print("1 argument:")
         print("1: {}".format(sys.argv[1]))
     else:
-        print("{:d} arguments:".format(argc))
+        print("{:d} arguments:".format(length))
         for i in range(1, length + 1):
             print("{}: {}".format(i, sys.argv[i]))
 
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import hideen_4
+import hidden_4
 
 
 if __name__ == "__main__":
