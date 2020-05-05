f404b34728e89f666e6a0091c28c0e3b1caac4cc
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-05-05 21:45:19 +0000

    copy

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -5,7 +5,7 @@ def print_matrix_integer(matrix=[[]]):
         for i in ele:
             c += 1
             if c != len(ele):
-                print("{:d}".format(i), end = ' ')
+                print("{:d}".format(i), end=' ')
             else:
-                print("{:d}".format(i), end = '')
+                print("{:d}".format(i), end='')
         print()
