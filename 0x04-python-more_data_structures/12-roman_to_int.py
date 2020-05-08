429efed8a419ce11226449770b4e70240a4f2d50
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-05-08 03:39:40 +0000

    scripts

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string:
-        return None
-    num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     total = 0
-    prev_val = 0
-    for i in range(len(roman_string)):
-        for key, val in num.items():
-            if key == roman_string[i]:
-                total += val
-                if prev_val < val:
-                    total -= (prev_val * 2)
-                    prev_val = val
+
+    if (not roman_string) or (type(roman_string) != str):
+        return None
+
+    for letter in range(len(roman_string)):
+        if letter == len(roman_string) - 1:
+            total += rom[roman_string[letter]]
+        elif d[roman_string[letter]] >= d[roman_string[letter + 1]]:
+            total += rom[roman_string[letter]]
+        else:
+            total -= rom[roman_string[letter]]
     return total
