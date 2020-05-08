5274764ddd1dfe4fd77d27603c81e4f296dda366
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-05-08 02:56:41 +0000

    scirpts

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    va = [True if search in my_list else False]
-    resu = list(map(lambda n: replace if n == search and va else n, my_list))
-    return result
+    resu = list(map(lambda n: replace if n == search else n, my_list))
+    return resu
