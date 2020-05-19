064791233558ebd0ec3987d17b1d612765ef2b06
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-05-19 04:16:25 +0000

    fix

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def safe_pprint_division(a, b):
+    div = 0
+    print("Inside result: ", end='')
+    try:
+        div = a / b
+    except:
+        div = None
+    finally:
+        print("{:d}".format(div))
+        return div
