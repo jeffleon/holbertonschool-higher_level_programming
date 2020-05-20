58f1a68aae8f6e83dc49e2b333091a1330b898f5
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-05-20 04:58:28 +0000

    fix 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -44,14 +44,14 @@ class Square:
             self.__position = value
 
     def my_print(self):
-        if (self.__position[1] > 0):
-            for i in range(self.__position[1]):
-                print()
         if self.__size > 0:
+            if (self.__position[1] > 0):
+                for i in range(self.__position[1]):
+                    print()
             for i in range(self.__size):
+                for k in range(self.__position[0]):
+                    print(' ', end='')
                 for j in range(self.__size):
-                    if j == 0 and self.__position[0] > 0:
-                        print(self.__position[0] * ' ', end='')
                     print('#', end='')
                 print()
         else:
