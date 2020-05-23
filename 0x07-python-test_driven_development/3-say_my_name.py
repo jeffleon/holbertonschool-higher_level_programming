9c9b829671f1d22133e78004be62a2e580465f80
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-05-23 18:47:23 +0000

    fix

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""
+Module contain say my name
+"""
 def say_my_name(first_name, last_name=""):
+    """
+    Say_my_name function
+    """
     errores = {'Typef': 'first_name must be a string',
                 'Typel': 'first_name must be a string'}
     if type(first_name) is not str:
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+"""
+a function of text_indentation
+"""
 def text_indentation(text):
+    """
+    if is not a text raise a error and if
+    the test is empty
+    """
     aux = 0
     if type(text) is not str:
         raise TypeError('must be a string')
@@ -10,9 +17,9 @@ def text_indentation(text):
         if aux == 1 and letter is ' ':
             print('', end='')
             aux = 0
-        elif letter is ':' or letter is '.' or letter is '?':
-            print(letter)
-            print()
+            continue
+        if letter is ':' or letter is '.' or letter is '?':
+            print("{}\n".format(letter))
             aux = 1
         else:
             print(letter, end='')
