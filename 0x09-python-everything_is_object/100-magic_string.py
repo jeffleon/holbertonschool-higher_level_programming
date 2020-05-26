7cec866903dbf9fe77b38d069c0360b93548c13c
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-05-26 23:40:17 +0000

    scripts

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,3 @@
+def magic_string(n = {'counter': 0}):
+    n['counter'] += 1
+    return "Holberton, " * (n['counter'] -1) + "Holberton" 
---
+++ 0x09-python-everything_is_object/crear.py
@@ -0,0 +1,6 @@
+import os
+
+
+for i in range(30):
+    file = open(str(i) + "-answer.txt", "w+")
+    file.close()
