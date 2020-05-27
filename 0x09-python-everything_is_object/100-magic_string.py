77887eb162ce1e63ef988a14a40347043aa132b5
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-05-27 01:36:54 +0000

    scripts

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
     magic_string.counter = getattr(magic_string, 'counter', 0) + 1
-    return ", ".join(["holberton" for counter in range(magic_string.counter)])
+    return ', '.join(["Holberton" for counter in range(magic_string.counter)])
