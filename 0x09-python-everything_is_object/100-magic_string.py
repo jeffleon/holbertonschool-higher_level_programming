54490edfe14c613871fceb7ca29c9dc2b437758e
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-05-27 00:19:30 +0000

    scripts

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def magic_string(n = {'counter': 0}):
     n['counter'] += 1
-    return "Holberton, " * (n['counter'] -1) + "Holberton" 
+    return "Holberton, " * (n['counter'] -1) + "Holberton"
