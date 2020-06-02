502051521c2fcc7c44874e4d425ae618c65d92c9
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-06-02 02:14:57 +0000

    scripts

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -7,7 +7,7 @@ def say_my_name(first_name, last_name=""):
     Say_my_name function
     """
     errores = {'Typef': 'first_name must be a string',
-                'Typel': 'first_name must be a string'}
+                'Typel': 'last_name must be a string'}
     if type(first_name) is not str:
         raise TypeError(errores.get('Typef'))
     if type(last_name) is not str:
