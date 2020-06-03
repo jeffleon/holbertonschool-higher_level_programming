4acfaa559a131093381f69b6811062fcb6bfb434
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-06-03 02:25:28 +0000

    add point 6

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""
+Class BaseGeometry
+"""
+class BaseGeometry:
+    """
+    Raise a Exception
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
