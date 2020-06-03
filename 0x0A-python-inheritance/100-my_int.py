5c0be2d6bac51906683454625f34d9f67a9957ee
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-06-03 04:19:16 +0000

    add 100

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Class Myint
+"""
+class Myint(int):
+    """
+    Init my int
+    """
+    def __init__(self, num):
+        self.num = num
+    def __eq__(self, numero):
+        return False
+    def __ne__(self, numero):
+        return True
