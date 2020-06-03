631e070dcad953fd04f579d6431ef8762788bca5
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-06-03 04:10:02 +0000

    python

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -34,6 +34,7 @@ class BaseGeometry:
         elif value <= 0:
             raise ValueError("{:s} must be greater than 0".format(name))
 
+
 class Rectangle(BaseGeometry):
     """
     Class Rectangle
