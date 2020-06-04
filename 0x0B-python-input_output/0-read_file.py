05279d8ff0dfdb34e370c3334db08dd22349c016
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-06-04 01:18:26 +0000

    scripts

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
+"""
+Handle Files
+"""
+
+
 def read_file(filename=""):
+    """
+    Function to print a File
+    """
     with open(filename) as f:
         for line in f:
             print(line, end="")
