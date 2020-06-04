412f617c2563302b73a3a25a70f97a8f7782075c
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-06-04 03:49:20 +0000

    fix 13

--- 0x0B-python-input_output/13-student.py
+++ 0x0B-python-input_output/13-student.py
@@ -1,1 +1,36 @@
 #!/usr/bin/python3
+"""
+Class Student
+"""
+
+
+class Student:
+    """
+    Constructor Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Attributes
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Return a Dictionary
+        """
+        if attrs is None:
+            return self.__dict__
+        dic = dict()
+        for atr in attrs:
+            if atr in self.__dict__:
+                dic[atr] = self.__dict__[atr]
+        return dic
+
+    def reload_from_json(self, json):
+        """
+        Replace all atributes
+        """
+        for key in json:
+            self.__dict__[key] = json[key]
