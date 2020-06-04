452ecb20cf8a8bf878115c98c6b5167b63670fba
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-06-04 04:46:24 +0000

    python 100

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def append_after(filename="", search_string="", new_string=""):
+    """
+    Function that add a new string determinates for a search
+    """
+    newline = []
+    with open(filename, "r+") as f:
+        for line in f:
+            newline.append(line)
+            if search_string in line:
+                newline.append(new_string)
+    f.close()
+    with open(filename, "w+") as f:
+        f.write("".join(newline))
