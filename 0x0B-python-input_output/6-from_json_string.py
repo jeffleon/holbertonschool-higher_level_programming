4853e5a04f28f52b759d6c8ee04fc7ac9e2bdbcc
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-06-04 01:58:02 +0000

    6 exersise

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+Handle Files
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    Return a cast to data struct in python
+    """
+    return json.loads(my_str)
