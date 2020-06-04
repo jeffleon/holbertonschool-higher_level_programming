ef58eba504d5ca1c8ecec588b560a8de04d7a25d
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-06-04 03:24:16 +0000

    scripts

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -10,8 +10,15 @@ class Student:
     Constructor Student
     """
     def __init__(self, first_name, last_name, age):
+        """
+        Attributes
+        """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
+
     def to_json(self):
+        """
+        Return a Dictionary
+        """
         return self.__dict__
