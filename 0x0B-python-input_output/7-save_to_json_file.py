107066276e63817b39041a855ec200048bd5ba42
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-06-04 02:04:54 +0000

    scripts

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""
+Handle Files
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    Write with json style
+    """
+    with open(filename, "w") as f:
+        f.write(json.dumps(my_obj))
