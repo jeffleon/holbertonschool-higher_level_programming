cc4f48b8dc244508ef9657daf5f356b60cf08660
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-06-04 04:52:50 +0000

    14

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+def pascal_triangle(n):
+    listaout = []
+    listain = []
+    if n <= 0:
+        return lista
+    for outs in range(n):
+        listain = [1]
+        for ins in range(outs):
+            listain.append(ins + 1)
+        listaout.append(listain[:])
