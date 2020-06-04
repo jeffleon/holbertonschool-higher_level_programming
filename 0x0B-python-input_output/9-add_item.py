1b482eb2d8498b8852f712f34dfac3019ed06dfa
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-06-04 02:52:13 +0000

    fix 9

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""
+Script to load and save
+"""
+import json
+import sys
+import os.path as path
+
+save = __import__('7-save_to_json_file').save_to_json_file
+load = __import__('8-load_from_json_file').load_from_json_file
+
+fname = "add_item.json"
+if path.exists(fname):
+    lista = load(fname)
+else:
+    lista = []
+
+if len(sys.argv) > 1:
+    for args in sys.argv[1:]:
+        lista.append(args)
+
+save(lista, fname)
