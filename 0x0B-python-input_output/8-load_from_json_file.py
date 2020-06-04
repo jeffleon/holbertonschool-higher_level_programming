1b6ad78592d7f469afaa254632e95c5dafcedb9d
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-06-04 02:13:48 +0000

    fix 8

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+Handle Json
+"""
+import json
+
+
+def load_from_json_file(filename):
+    """
+    Return a obj_json
+    """
+    with open(filename) as f:
+        for line in f:
+            obj = json.loads(line)
+    return obj
