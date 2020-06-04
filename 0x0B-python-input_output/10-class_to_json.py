1677387ba49b3af4d5e2a9f740003f7cfec0dc1f
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-06-04 03:10:39 +0000

    fix 10

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -5,7 +5,7 @@ Handle Files
 
 
 def class_to_json(obj):
-       """
-       Return a Dictionary with attributes
-       """
-       return obj.__dict__
+    """
+    Return a Dictionary with attributes
+    """
+    return obj.__dict__
