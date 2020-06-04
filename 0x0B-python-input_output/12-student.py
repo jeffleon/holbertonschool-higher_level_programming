c04d8097a826839bbfb98709c524e1da1ad92dd6
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-06-04 03:42:18 +0000

    fix 12

--- 0x0B-python-input_output/12-student.py
+++ 0x0B-python-input_output/12-student.py
@@ -1,1 +1,29 @@
 #!/usr/bin/python3
+"""
+Class Student
+"""
+
+
+class Student:
+    """
+    Constructor Student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Attributes
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Return a Dictionary
+        """
+        if attrs == None:
+            return self.__dict__
+        dic = dict()
+        for atr in attrs:
+            if atr in self.__dict__:
+                dic[atr] = self.__dict__[atr]
+        return dic
