1aa62a180e8386f799441975ef4fd70eb2483fe9
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-06-04 05:11:36 +0000

    fix 2

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -3,29 +3,15 @@
 Handle Files
 """
 
-
-def number_of_lines(filename=""):
-    """
-    Return the number of lines in file
-    """
-    num = 0
-    with open(filename) as f:
-        for line in f:
-            num += 1
-    f.close
-    return num
-
-
 def read_lines(filename="", nb_lines=0):
     """
     Read a especific lines of code
     """
-    lines = number_of_lines(filename)
-    if nb_lines <= 0:
-        nb_lines = lines
     with open(filename) as f:
-        for n, line in enumerate(f):
-            if n == nb_lines:
-                break
-            print(line)
-    f.close()
+        lines = f.readlines()
+        if nb_lines <= 0 or nb_lines >= len(lines):
+            for line in lines:
+                print(line, end='')
+        else:
+            for no_line in range(nb_lines):
+                print(lines[no_line], end='')
