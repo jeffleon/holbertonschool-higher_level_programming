d52150a7f3a1c369850dd927ca8d430c9d48db92
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-06-09 22:09:10 +0000

    fix square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,15 +9,18 @@ class Square(Rectangle):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
+        """docstring"""
         return '[Square] ({}) {}/{} - {}'.format(self.id, self.x, self.y,
                                                  self.width)
 
     @property
     def size(self):
+        """docstring"""
         return self.width
 
     @size.setter
     def size(self, value):
+        """docstring"""
         if type(value) is not int:
             raise TypeError('width must be an integer')
         if value <= 0:
@@ -26,6 +29,7 @@ class Square(Rectangle):
         self.height = value
 
     def update(self, *args, **kwargs):
+        """docstring"""
         lista = ['id', 'size', 'x', 'y']
         for en, a in enumerate(args):
             setattr(self, lista[en], a)
@@ -36,4 +40,5 @@ class Square(Rectangle):
                     break
 
     def to_dictionary(self):
+        """docstring"""
         return {'id': self.id, 'x': self.x, 'size': self.width, 'y': self.y}
