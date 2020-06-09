77c2035120ae0fb42715e753b2b3f888a58174c4
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-06-09 22:25:14 +0000

    fix square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -23,7 +23,7 @@ class Square(Rectangle):
         """docstring"""
         if type(value) is not int:
             raise TypeError('width must be an integer')
-        if value <= 0:
+        if value < 0:
             raise ValueError('width must be >= 0')
         self.width = value
         self.height = value
