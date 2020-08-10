5f0f757555e26435c4e8a572754c1964b85eedb0
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-08-10 03:52:28 +0000

    add task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 import MySQLdb
 import sys
-db = MySQLdb.connect(host="127.0.0.1", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-cursor = db.cursor()
-cursor.execute("select * from states where name like 'N%'")
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
-db.close()
+""" main function"""
+if __name__ == '__main__':
+    """ conect a db and query to elements that begin with letter N"""
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("select * from states where name like 'N%'")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    db.close()
