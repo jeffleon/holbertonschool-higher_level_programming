f0edc0424dfa18f738a4572285a4e96796674ded
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-08-10 04:03:13 +0000

    task1.

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """ main function"""
 if __name__ == '__main__':
-    """ conect a db and query to elements that begin with letter N"""
     import MySQLdb
     import sys
+    """ conect a db and query to elements that begin with letter N"""
     db = MySQLdb.connect(host="localhost", port=3306,
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("select * from states where name like 'N%' order by id ASC")
+    cursor.execute("select * from states where name like binary 'N%'")
     rows = cursor.fetchall()
     for row in rows:
         print(row)
