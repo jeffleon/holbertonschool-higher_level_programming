8535a1770f371ce0c48076abff76a3bbcc282d10
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-08-10 04:56:30 +0000

    add task 14

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-import sys
-from model_state import Base, State
-from model_city import Base, City
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine, join
-
+""" function main """
 if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from model_city import Base, City
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine, join
+    """ main """
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
     Session = sessionmaker(engine)
