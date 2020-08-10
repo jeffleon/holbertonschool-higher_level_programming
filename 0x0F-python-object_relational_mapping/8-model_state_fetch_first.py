771a49ca3ab590b6b41708e230e80ad683437b93
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-08-10 04:44:28 +0000

    task 9

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+""" main function """
 if __name__ == "__main__":
     import sys
     from model_state import Base, State
     from sqlalchemy.orm import sessionmaker
     from sqlalchemy import (create_engine)
+    """ database """
     value = "mysql+mysqldb://{}:{}@localhost/{}"\
                            "".format(sys.argv[1], sys.argv[2],
                                      sys.argv[3]), pool_pre_ping=True)
