01f786a1a147e22dde7171017749a99c51f2bfbf
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-08-10 03:48:33 +0000

    task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
 """ select all rows """
 if __name__ == '__main__':
+    import MySQLdb
+    import sys
     """ Arguments """
-    db = MySQLdb.connect(host="127.0.0.1",
+    db = MySQLdb.connect(host="localhost",
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     cursor.execute("select * from states")
