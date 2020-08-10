1e3aa21bc07ed0cfc4aab7554a5665ea6b171ea8
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-08-10 04:25:13 +0000

    task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
-
-db = MySQLdb.connect(host="127.0.0.1", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-cursor = db.cursor()
-cursor.execute("select cities.name "
-               "from cities inner join states "
-               "on cities.state_id = states.id "
-               "where states.name = %s " 
-               "order by cities.id ASC;", (sys.argv[4],))
-result = list()
-results = cursor.fetchall()
-for tuple_ in results:
-    if tuple_[0] not in result:
-        result.append(tuple_[0])
-print(", ".join(result))
-cursor.close()
-db.close()
+""" function main """
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+    """ database """
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("select cities.name "
+                   "from cities inner join states "
+                   "on cities.state_id = states.id "
+                   "where states.name = %s " 
+                   "order by cities.id ASC;", (sys.argv[4],))
+    result = list()
+    results = cursor.fetchall()
+    for tuple_ in results:
+        if tuple_[0] not in result:
+            result.append(tuple_[0])
+    print(", ".join(result))
+    cursor.close()
+    db.close()
