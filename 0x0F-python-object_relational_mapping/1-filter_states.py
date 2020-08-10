4ec3352c0a2404a88922d555ed8eaa7c48fe569c
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-08-10 03:53:58 +0000

    add task 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
 """ main function"""
 if __name__ == '__main__':
     """ conect a db and query to elements that begin with letter N"""
+    import MySQLdb
+    import sys
     db = MySQLdb.connect(host="localhost",
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
