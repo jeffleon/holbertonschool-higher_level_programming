96b0fa7efc193fdca33aeca6f03977501dd195b4
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-08-10 04:19:27 +0000

    add task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
-
-db = MySQLdb.connect(host="127.0.0.1", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-cursor = db.cursor()
-query = "select * from states where name = %s;"
-cursor.execute(query, (sys.argv[4],))
-results = cursor.fetchall()
-for result in results:
-    print(result)
-cursor.close()
-db.close()
+""" function main """
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+    """ database """
+    db = MySQLdb.connect(host="localhost",
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    query = "select * from states where name = %s;"
+    cursor.execute(query, (sys.argv[4],))
+    results = cursor.fetchall()
+    for result in results:
+        print(result)
+    cursor.close()
+    db.close()
