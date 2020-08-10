d3d8a6950a7ffe3fea5faac1adb5abdfd2cfbe0c
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-08-10 04:14:21 +0000

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,10 +4,11 @@ if __name__ == '__main__':
     import MySQLdb
     import sys
     """ find a coincidence between database and typed arg"""
-    db = MySQLdb.connect(host="127.0.0.1", user=sys.argv[1],
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    query = "select * from states where name = '{}'".format(sys.argv[4])
+    query = "select * from states where name like binary '{}'"\
+    .format(sys.argv[4])
     cursor.execute(query)
     results = cursor.fetchall()
     for result in results:
