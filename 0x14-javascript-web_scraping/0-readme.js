2f58cb44661624f56b4adcc239b2586492dc9650
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-09-22 23:30:32 +0000

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFileSync(process.argv[2], 'utf8', (error, data) => {
+fs.readFile(process.argv[2], 'utf8', (error, data) => {
   if (error) { console.log(error); }
   if (!error) { console.log(data); }
 });
