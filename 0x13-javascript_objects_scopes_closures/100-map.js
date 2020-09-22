5141db6114cc8facd2a3ec149b42632ec15752bf
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-09-22 03:43:36 +0000

    add task 100

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
 
-new_list = list.map((element, index)=>{
-    return element * index
-})
-console.log(list)
-console.log(new_list);
+const NewList = list.map((element, index) => {
+  return element * index;
+});
+console.log(list);
+console.log(NewList);
