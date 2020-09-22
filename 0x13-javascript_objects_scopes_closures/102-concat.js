252c93b3f6e323377f67eb399d6bedc22de4e5a7
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-09-22 03:57:32 +0000

    task 13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,23 +2,15 @@
 
 const fs = require('fs');
 function readfile (file) {
-  try {
-    const data = fs.readFileSync(file, 'utf8');
-    return data;
-  } catch (err) {
-    console.error(err);
-  }
+  const data = fs.readFileSync(file, 'utf8');
+  return data;
 }
 function writefile (path, content) {
-  try {
-    const data = fs.writeFileSync(path, content);
-    return data;
-  } catch (err) {
-    console.error(err);
-  }
+  const data = fs.writeFileSync(path, content, (err) => console.log(err));
+  return data;
 }
 
 const data = readfile(process.argv[2]);
 const data1 = readfile(process.argv[3]);
-const content = data + '\n' + data1 + '\n';
+const content = data + data1;
 writefile(process.argv[4], content);
