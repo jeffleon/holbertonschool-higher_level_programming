e71333e741607736ab0a22b586515879152e0096
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-09-22 04:04:21 +0000

    task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-
 exports.nbOccurences = function (list, searchElement) {
   let nvar = 0;
   for (let i = 0; i < list.length; i++) {
     if (list[i] === searchElement) {
-      nvar += 1;
+      nvar++;
     }
   }
   return nvar;
