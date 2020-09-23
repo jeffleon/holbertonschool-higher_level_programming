c72449613261eb1ce2769aae7a95042809271966
jeffleon <jefferson.leon@usantotomas.edu.co>
2020-09-23 00:12:08 +0000

    task 100

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/films', (error, response, body) => {
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], (error, response, body) => {
   if (error) { console.log(error); }
   const json = JSON.parse(body);
-  const episode = json.results.filter((element) => parseInt(element.episode_id) === parseInt(process.argv[2]));
-  episode[0].characters.forEach((url) => {
+  json.characters.forEach((url) => {
     request(url, (error, response, body) => {
       if (error) { console.log(error); }
       const json = JSON.parse(body);
